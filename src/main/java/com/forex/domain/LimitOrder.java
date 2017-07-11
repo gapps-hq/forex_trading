250085190bbf6c22caa5a286c4c56c8f66b9a79f
Java <gapps.hq@gmail.com>
2017-07-11 06:52:44 -0700

    Added the LimitOrder Controller

--- src/main/java/com/forex/domain/LimitOrder.java
+++ src/main/java/com/forex/domain/LimitOrder.java
@@ -2,40 +2,81 @@ package com.forex.domain;
 
 public class LimitOrder {
 
-	private Currency currency;
-    private double amount;
-    private Side side;
+    private double limit_price;
+    private Currency currency;
+   private double lot_size;
+   private Side side;
+   private double price;
 
-    
-    public LimitOrder(Currency currency, double amount, Side side) {
+    String transaction_time;
+   String type_of_order;
+   String status;
+
+   public double getLot_size() {
+        return lot_size;
+    }
+
+    public void setLot_size(double lot_size) {
+        this.lot_size = lot_size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTransaction_time() {
+        return transaction_time;
+    }
+
+    public void setTransaction_time(String transaction_time) {
+        this.transaction_time = transaction_time;
+    }
+
+    public String getType_of_order() {
+        return type_of_order;
+    }
+
+    public void setType_of_order(String type_of_order) {
+        this.type_of_order = type_of_order;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+   
+   public LimitOrder() {
+   }
+
+    public double getLimit_price() {
+        return limit_price;
+    }
+
+    public void setLimit_price(double limit_price) {
+        this.limit_price = limit_price;
+    }
+   
+   public Currency getCurrency() {
+       return currency;
+   }
+
+   public Side getSide() {
+       return side;
+   }
+
+    public void setCurrency(Currency currency) {
         this.currency = currency;
-        this.amount = amount;
+    }
+
+    public void setSide(Side side) {
         this.side = side;
     }
-
-    public Currency getCurrency() {
-        return currency;
-    }
-
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public Side getSide() {
-        return side;
-    }
-
-	public void setCurrency(Currency currency) {
-		this.currency = currency;
-	}
-
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
-
-	public void setSide(Side side) {
-		this.side = side;
-	}
-    
+   
 }
